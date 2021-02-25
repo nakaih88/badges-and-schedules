35c6212bc214dade3fe3ca795eb6679175e1be09
Nakai <nakai.hamlin18@gmail.com>
2021-02-24 20:31:41 -0500

    commit tree 9d678d429e981f4ff6eeaf784e40aae7c9e17f8d

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def badge_maker(name)
+    "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+    array_1 = []
+    names.each do |name|
+        array_1 <<(badge_maker(name))
+    end
+    return array_1
+end
+
+def assign_rooms(speakers)
+    array_2 = []
+    speakers.each.with_index(1) do |name, index|
+        array_2 << "Hello, #{name}! You'll be assigned to room #{index}!"
+    end
+    array_2
+end
+
+def printer(attendees)
+    batch_badge_creator(attendees).each do | badge |
+        puts badge
+    end
+    assign_rooms(attendees).each do | speaker|
+        puts speaker
+    end
+end
